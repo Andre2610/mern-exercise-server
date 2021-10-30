bd8a90a8cdcac625630fc36e50c2bcddb98fc3e5
andre2610 <dre2610@gmail.com>
2021-10-30 18:09:09 +0200

    installed dependencies and created express app

---
+++ config/constants.js
@@ -0,0 +1,3 @@
+require("dotenv").config;
+
+export const CONNECTION_URL = process.env.CONNECTION_URL;
--- index.js
+++ index.js
@@ -2,3 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import { CONNECTION_URL } from "./config/constants";
+
+const app = express();
+const jsonParser = express.json();
+
+app.use(jsonParser);
+app.use(cors());
