6bf88e0b2494fc23e4f59c025753dd26e8f902c5
andre2610 <dre2610@gmail.com>
2021-11-01 18:17:58 +0100

    created update and delete endpoints

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res, next) => {
@@ -22,6 +23,64 @@ export const createPost = async (req, res, next) => {
     await newPost.save();
 
     res.status(201).send(newPost);
+  } catch (error) {
+    console.log("Error", error.message);
+    res.status(409).send({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res, next) => {
+  try {
+    const { id: _id } = req.params;
+    const { data } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id))
+      return res.status(404).send({ message: "Post not found" });
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, data, {
+      new: true,
+    });
+
+    res.status(200).send(updatedPost);
+  } catch (error) {
+    console.log("Error", error.message);
+    res.status(409).send({ message: error.message });
+  }
+};
+
+export const deletePost = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send({ message: "Post not found" });
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.status(200).send({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.log("Error", error.message);
+    res.status(409).send({ message: error.message });
+  }
+};
+
+export const likePost = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send({ message: "Post not found" });
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(
+      id,
+      { likeCount: post.likeCount + 1 },
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).send(updatedPost);
   } catch (error) {
     console.log("Error", error.message);
     res.status(409).send({ message: error.message });
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { getPosts, createPost } from "../controllers/posts.js";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} from "../controllers/posts.js";
 
 const router = new Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.patch("/:id/like-post", likePost);
 
 export default router;
